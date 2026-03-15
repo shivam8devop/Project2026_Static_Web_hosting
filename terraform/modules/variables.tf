8b9ac0ff4b4da2b4c48481839c607fa9e3d0a247
shivam8devop <shivam8.singh@outlook.com>
2026-03-15 20:05:20 +0530

    resuming previous work

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -1,3 +1,4 @@
-variable "iam_user_name" {}
-variable "bucket_name" {}
-variable "table_name" {}
+variable "iam_user_name"{}
+variable "bucket_name"{}
+variable "table_name"{}  
+variable "region"{}
